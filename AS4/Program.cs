8238eb02b6451e117544596b91c11519e6407739
DIpesh Rawal <dipeshrawal007@gmail.com>
2026-07-13 18:41:00 +0545

    Delegate update

---
+++ AS4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+delegate void Message(string message);
+
+class AS4
+{
+    static void SendEmail(string message)
+    {
+        Console.WriteLine("Email: " + message);
+    }
+
+    static void SendSMS(string message)
+    {
+        Console.WriteLine("SMS: " + message);
+    }
+
+    static void Main()
+    {
+        Message m = null;
+
+        m += SendEmail;
+        m += SendSMS;
+
+        Console.WriteLine("After adding delegates:");
+        m("Welcome!");
+        m -= SendSMS;
+
+        Console.WriteLine("\nAfter removing SMS delegate:");
+       m("Hello Again!");
+    }
+}
---
+++ AS4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AS4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("AS4")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f24b37f1-bb33-4456-b4d5-71a8a398db5f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
