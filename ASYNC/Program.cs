853c6c407421d53abd37cc32a1132eed6d51c544
DIpesh Rawal <dipeshrawal007@gmail.com>
2026-07-16 14:44:29 +0545

    asynchronous await

---
+++ ASYNC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class ASYNC
+{
+    static async Task Main()
+    {
+        int result = await AddAsync(5, 3);
+
+        Console.WriteLine(result);
+    }
+
+    static async Task<int> AddAsync(int a, int b)
+    {
+        await Task.Delay(1000); 
+        return a + b;
+    }
+}
