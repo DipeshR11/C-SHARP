cd20746a0effeb6e9fdbab5fc5cc1115c0fb2234
DIpesh Rawal <dipeshrawal007@gmail.com>
2026-07-26 20:28:43 +0545

    some changes

---
+++ WebApp/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webproject.Models;
+namespace webproject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            return View("Detail", product);
+        }
+        public IActionResult Save()
+        {
+            return View();
+        }
+    }
+}
