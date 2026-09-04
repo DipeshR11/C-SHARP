3dfb53cffc3958b417da12546f1969a86a650c40
DIpesh Rawal <dipeshrawal007@gmail.com>
2026-09-04 20:03:38 +0545

    Updated C-SHARP project

--- WebAppp/Models/Contact.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppp.Models
-{
-    public class Contact
-    {
-        public string? Email { get; set; }
-        public string? Subject { get; set; }
-        public string? Message { get; set; }
-    }
-}
